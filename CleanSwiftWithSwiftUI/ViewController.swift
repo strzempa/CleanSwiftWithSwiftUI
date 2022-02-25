d4eb8f3a79aec3362a02a5c0dc6beb1dc5930db3
Patryk Strzemiecki <strzempa@gmail.com>
2022-02-25 22:32:35 +0100

    Initial Commit

---
+++ CleanSwiftWithSwiftUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanSwiftWithSwiftUI
+//
+//  Created by Patryk Strzemiecki on 25/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
